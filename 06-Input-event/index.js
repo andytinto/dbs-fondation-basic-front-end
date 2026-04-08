9452d8d1d862dca057888502ba51c667b3137a36
andy.kristanto <aktdev.id@gmail.com>
2026-04-08 15:12:55 +0700

    feat: add onfocus event

--- 06-Input-event/index.js
+++ 06-Input-event/index.js
@@ -19,4 +19,9 @@ document.addEventListener('DOMContentLoaded', function() {
 			document.getElementById('notifikasiSisaKarakter').style.color = 'black';
 		}
 	});
+
+	document.getElementById('inputNama').addEventListener('focus', function() {
+		console.log('inputNama: focus');
+		document.getElementById('notifikasiSisaKarakter').style.visibility = 'visible';
+	});
 });
